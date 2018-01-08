642842f304c05f77f0900c785757ad5d893d4e91
PartycjaK <klis.patrycja92@gmail.com>
2018-01-08 17:25:31 +0100

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,53 @@
+document.getElementById("button").addEventListener("click", init);
+
+const plnPln = 1.0;
+const usdUsd = 1.0;
+const eurEur = 1.0;
+const usdPln = 3.45;
+const plnUsd = 0.29;
+const plnEur = 0.24;
+const eurPln = 4.15;
+const usdEur = 1.20;
+const eurUsd = 0.83;
+var choosenCurrencyFrom = document.getElementById("sel-opt");
+var choosenCurrencyTo = document.getElementById("sel-opt2");
+
+var showResult = document.querySelector(".result");
+
+function init(e) {
+    e.preventDefault();
+    var input = document.getElementById("your-val").value;
+    var number = Number(input);
+    calculate(input);
+}
+
+function calculate(givenValue) {
+    var currencyFrom = choosenCurrencyFrom.value;
+    var currencyTo = choosenCurrencyTo.value;
+    var answer;
+    const pln = "PLN";
+    const usd = "USD";
+    const eur = "EUR";
+
+    if (currencyFrom === pln && currencyTo === usd) {
+        answer = givenValue * plnUsd;
+    } else if (currencyFrom === pln && currencyTo === eur) {
+        answer = givenValue * plnEur;
+    } else if (currencyFrom === pln && currencyTo === pln) {
+        answer = givenValue * plnPln;
+    } else if (currencyFrom === usd && currencyTo === pln) {
+        answer = givenValue * usdPln;
+    } else if (currencyFrom === usd && currencyTo === eur) {
+        answer = givenValue * usdEur;
+    } else if (currencyFrom === usd && currencyTo === usd) {
+        answer = givenValue * usdUsd;
+    } else if (currencyFrom === eur && currencyTo === pln) {
+        answer = givenValue * eurPln;
+    } else if (currencyFrom === eur && currencyTo === usd) {
+        answer = givenValue * eurUsd;
+    } else if (currencyFrom === eur && currencyTo === eur) {
+        answer = givenValue * eurEur;
+    }
+
+    showResult.textContent = `${givenValue} ${currencyFrom} = ${answer.toFixed(2)} ${currencyTo}`;
+}
